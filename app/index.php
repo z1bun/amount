8c4ad021b611363c9189699896d2133b819c3a9d
Артем Константинов <artem@konstantinov.xyz>
2018-07-03 11:57:34 +0300

    finish the app, update README

--- app/index.php
+++ app/index.php
@@ -4,7 +4,53 @@ require_once 'models/Amount.php';
 
 use app\models\Amount;
 
-$amount = new Amount();
-echo $amount->Base();
+$amount = new Amount(60, 15, 10);
+try {
+    if ($amount->diff()) {
+        echo 'Отклонение допустимое';
+    } else {
+        echo 'Отклонение недопустимое';
+    }
+    echo "\n";
+    echo 'Отклонение: ' . $amount->getAmount();
+    echo "\n";
+} catch (\Throwable $e)
+{
+    $e->getMessage();
+}
+
+unset($amount);
+
+$amount = new Amount(5, 15, 10);
+try {
+    if ($amount->diff()) {
+        echo 'Отклонение допустимое';
+    } else {
+        echo 'Отклонение недопустимое';
+    }
+    echo "\n";
+    echo 'Отклонение: ' . $amount->getAmount();
+    echo "\n";
+} catch (\Throwable $e)
+{
+    $e->getMessage();
+}
+
+unset($amount);
+
+$amount = new Amount(5, 15, 0);
+try {
+    if ($amount->diff()) {
+        echo 'Отклонение допустимое';
+    } else {
+        echo 'Отклонение недопустимое';
+    }
+    echo "\n";
+    echo 'Отклонение: ' . $amount->getAmount();
+    echo "\n";
+} catch (\Throwable $e)
+{
+    echo 'Возникла исключительная ситуация: ' . $e->getMessage();
+}
 
 ?>
--- app/models/Amount.php
+++ app/models/Amount.php
@@ -2,21 +2,75 @@
 
 namespace app\models;
 
-
+/**
+ * Class Amount
+ * @package app\models
+ *
+ * Класс для контроля колебания цены
+ *
+ * @property int $tolerance Допустимое отклонение в %
+ * @property int $currentPrice Текущая цена
+ * @property int $oldPrice Старая цена
+ * @property float $result - Результат вычислений
+ */
 class Amount
 {
 
-    /**
-     * Amount constructor.
-     */
-    public function __construct()
-    {
+    /** @var int Допустимое отклонение в % */
+    private $tolerance;
 
+    /** @var int Текущая цена */
+    private $currentPrice;
+
+    /** @var int Предыдущая цена */
+    private $oldPrice;
+
+    /** @var float Результат расчета в % */
+    private $result;
+
+    /**
+     * Конструктор класса
+     *
+     * @param int $tolerance - допустимое отклонение в %
+     * @param int $currentPrice - текущая цена
+     * @param int $oldPrice - предыдущая цена
+     * @param float $result - результат в %
+     */
+    public function __construct(int $tolerance, int $currentPrice, int $oldPrice = 0, float $result = 0)
+    {
+        $this->tolerance = $tolerance;
+        $this->currentPrice = $currentPrice;
+        $this->oldPrice = $oldPrice;
+        $this->result = $result;
     }
 
-    public function Base()
+    /**
+     * Проверяет, является ли разница между текущей ценой и старой допустимой по отклонению
+     *
+     * @return bool
+     * @throws \DivisionByZeroError
+     */
+    public function diff(): bool
     {
-        return 'Hello';
+        if ($this->oldPrice !== 0) {
+            $diff = ((float)$this->currentPrice / (float)$this->oldPrice - 1) * 100;
+            $this->result = $diff;
+
+            return (float)$this->tolerance > abs($this->result);
+        }
+
+        throw new \DivisionByZeroError('Невозможно вычислить разницу. Деление на ноль');
+    }
+
+    /**
+     * Возвращает результат вычисления
+     *
+     * @return int
+     */
+    public function getAmount(): int
+    {
+        /** на случай, если число слишком большое, округляем его */
+        return round($this->result, 2);
     }
 
 }
